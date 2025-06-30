2f93a04b3013047bd716298dac3c59c900c61298
sebaslade <sebaslade29@gmail.com>
2025-06-29 20:42:33 -0500

    fix: Iterando codigo del registro

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import {useForm} from "react-hook-form";
-import axios,{isAxiosError} from "axios";
+import {isAxiosError} from "axios";
+import {toast} from "sonner";
 import type { RegisterForm } from "../types";
 import ErrorMessage from "../../components/ErrorMessage";
+import api from "../config/axios";
 
 export default function RegisterView() {
-
     const initialValues: RegisterForm = {
         name: "",
         email: "",
@@ -20,12 +21,13 @@ export default function RegisterView() {
 
     const handleRegister = async (formData: RegisterForm) => {
         try {
-            const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData);
-            console.log(data);
+            const {data} = await api.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData);
+            //console.log(data);
+            toast.success(data)
             reset()
         } catch (error) {
             if(isAxiosError(error) && error.response){
-                console.log(error.response?.data.error)
+                toast.error(error.response?.data.error)
             }
         }
     }
