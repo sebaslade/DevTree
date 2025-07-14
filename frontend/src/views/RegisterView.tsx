6795ac16e28eb6225371c88e4d337f8014bd3aeb
sebaslade <sebaslade29@gmail.com>
2025-07-14 13:12:11 -0500

    fix: Redireccionamiento del registro

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {useForm} from "react-hook-form";
 import {isAxiosError} from "axios";
 import {toast} from "sonner";
@@ -7,10 +7,12 @@ import ErrorMessage from "../components/ErrorMessage";
 import api from "../config/axios";
 
 export default function RegisterView() {
+    const location = useLocation();
+    const navigate = useNavigate();
     const initialValues: RegisterForm = {
         name: "",
         email: "",
-        handle: "",
+        handle: location?.state?.handle || "",
         password: "",
         password_confirmation: ""
     };
@@ -25,6 +27,7 @@ export default function RegisterView() {
             //console.log(data);
             toast.success(data)
             reset()
+            navigate('/auth/login')
         } catch (error) {
             if(isAxiosError(error) && error.response){
                 toast.error(error.response?.data.error)
